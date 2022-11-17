e07ba61ef9f53b06be07a9394bc49458d239637e
pyguru123 <pyguru123@gmail.com>
2022-11-17 23:15:24 +0530

    added remove bg

---
+++ Awesome python libraries/Remove Image Background/script.py
@@ -0,0 +1,16 @@
+# Remove image background
+# pip install rembg
+
+import rembg
+import os
+
+for index, file in enumerate(os.listdir()):  
+	print(index, file)
+	if file.endswith('.jpg'):
+		name, ext = os.path.splitext(file)
+		with open(file, "rb") as inp:
+			with open(f"{name}-rem.png", "wb") as out:
+				input = inp.read()
+				output = rembg.remove(input)
+				out.write(output)
+		print(f'done {index+1}')
